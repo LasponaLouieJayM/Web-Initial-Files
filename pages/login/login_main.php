3346ad5bdd9298d9c1d5045e8023cc90ef795471
LouieJay <louizkylaspona@gmail.com>
2023-10-04 23:56:19 +0800

    Login

--- pages/login/login_main.php
+++ pages/login/login_main.php
@@ -20,6 +20,35 @@
                     <p1>Remember me</p1>
                     <a href="#">Forgot password?</a>
                 </div>
+                <!-- Button to Open the Modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+  Open modal
+</button>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
                 <button onclick="to_home()" name="submit" class="submit">Login</button>
                 <div class="register-link">
                     <p>Don't have an account? <div class="text-dark" ></p>
